b4563defea85b2c8a799ea9c240b2895e3aeded4
DamianGallego <damiangallego09@gmail.com>
2018-04-16 18:05:14 -0300

    * Se levanta app

--- src/main/java/com/bolivarSoftware/sie/web/HomeController.java
+++ src/main/java/com/bolivarSoftware/sie/web/HomeController.java
@@ -15,4 +15,10 @@ public class HomeController {
     public String inicio(){
         return "index";
     }
+
+
+    @RequestMapping(value = {"login"})
+    public String login(){
+        return "login";
+    }
 }
