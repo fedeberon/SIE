d9f04a7f165889e876c65a29fce1ccad9e6747ac
DamianGallego <damiangallego09@gmail.com>
2018-04-16 17:32:36 -0300

    * Estructura web. * Templeate.

---
+++ src/main/java/com/bolivarSoftware/sie/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.bolivarSoftware.sie.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Damian Gallego on 16/4/2018.
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @RequestMapping(value = {"/", "index", "inicio"})
+    public String inicio(){
+        return "index";
+    }
+}
